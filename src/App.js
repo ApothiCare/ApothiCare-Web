25e871c4f440f3cf7be7c8cc3a91e4e85ecfb879
Thomas PEN <thomas.pen@epitech.eu>
2019-02-04 19:15:55 +0100

    First draft of new showcase website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,171 @@
 import React, { Component } from 'react';
+import "react-grid-layout/css/styles.css";
+import "react-resizable/css/styles.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars} from "@fortawesome/free-solid-svg-icons";
+
 import logo from './logo.svg';
 import './App.css';
+import TestGrid from './TestGrid';
 
+//use Json formatted files
+ // Each json element is defined by its activity, users and fonctionalities are secondary
+//define default colors for each nth array inside an array (2nd user would be red for example)
+let activities = {
+  name: "Scanner ses médicaments",
+  users: ["Utilisateur de l'application"],
+  goals: [
+      "S'assurer de l'authenticité du médicament",
+      "Vérifier la date de péremption",
+      "Accéder à la notice du médicament"
+  ]
+};
 class App extends Component {
+  constructor(props) {
+    super(props);
+    window.addEventListener('scroll', this.handleScroll);
+    this.state = {
+      TopNavbarStyle : {}
+    };
+  }
+
+  //region Responsive + DownScrollTopNavbar transformation
+  handleScroll = (e) => {
+    var top = (window.scrollY < 5) ? true : false;
+    var tmp = {
+      TopNavbarStyle: (top) ? {
+        'background-color':  'transparent',
+        'color' : 'black',
+      } : {
+        'background-color':  'white',
+        'color' : '',
+        'border-bottom': '1px solid grey',
+        'box-shadow': '0px 2px 5px grey',
+      }
+    };
+    this.setState(tmp);
+  };
+
+  goMobile() {
+    let x = document.getElementById("TOPNAV#1");
+    if (x.className === "top-nav") {
+      x.className += " responsive";
+    } else {
+      x.className = "top-nav";
+    }
+  }
+  //endregion
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+
+        <div className={"top-nav"} id={"TOPNAV#1"} style={this.state.TopNavbarStyle} >
+          <a href={"#home"} className={"logo"}>
+            <img
+                src={"http://res.cloudinary.com/hrscywv4p/image/upload/c_limit,fl_lossy,h_300,w_300,f_auto,q_auto/v1/1562599/smartphone_ganvug.png"}
+                width={75}
+                height={75}/>
+            <h2>ApothiCare</h2>
           </a>
-        </header>
+          <a className={"nav-items"} href={"#home"} onClick={() => {
+            window.scrollTo(0, 0);
+          }}>
+            <h3>Home</h3>
+          </a>
+          <a className={"nav-items"} href={"#solution"} onClick={() => {
+            window.scrollTo(0, 800);
+          }}>
+            <h3>Notre solution</h3>
+          </a>
+          <a className={"nav-items"} href={"#contact"} onClick={() => {
+            window.scrollTo(0, 1800);
+          }}>
+            <h3>Nous contacter</h3>
+          </a>
+          <a href="javascript:void(0);" className="icon" onClick={this.goMobile}>
+            <FontAwesomeIcon icon={faBars}></FontAwesomeIcon>
+          </a>
+        </div>
+
+        <div className={"header"}>
+          <div className={"header-content"}>
+            <div className={"header-content-title"}>
+              Simplifiez la gestion de vos médicaments<br/>
+            </div>
+            <div
+                className={"header-content-subtitle"}>
+              ApothiCare, une solution innovante permettant aux particuliers de gérer de de façon numérique leurs medicaments
+            </div>
+            <div className={"header-content-button"}>En apprendre plus</div>
+          </div>
+        </div>
+
+        <div className={"content"}>
+          <div className={"content-title"}>Notre solution</div>
+          <div className={"content-subtitle"}>Une application mobile de gestion de vos médicaments.</div>
+          <div className={"content-content-grid"}>
+            <div className={"card"}>
+              <img className={"void"}></img>
+              <div className={"text-card"}>
+                <div className={"text-card-title"}>
+                  GESTION
+                </div>
+                <div className={"text-card-subtitle"}>
+                  Gérez votre stock de médicaments et vos traitements !
+                </div>
+                <div className={"text-card-content"}>Scannez le QR code de votre boite de médicament pour obtenir toutes les informations disponibles !
+                  Il est automatiquement ajouté à l'application pour vous informer en cas de péremption ou de retrait de lot.</div>
+              </div>
+            </div>
+            <div className={"card"}>
+              <div className={"text-card"}>
+                <div className={"text-card-title"}>
+                  RAPPEL
+                </div>
+                <div className={"text-card-subtitle"}>
+                  N'oubliez plus de prendre vos médicaments !
+                </div>
+                <div className={"text-card-content"}>
+                  Vous pouvez mettre en place des alarmes personnalisées et vous êtes automatiquement prévenu lorsque votre stock est faible.
+                </div>
+              </div>
+              <img className={"void"}></img>
+            </div>
+            <div className={"card"}>
+              <img className={"void"}></img>
+              <div className={"text-card second"}>
+                <div className={"text-card-title"}>
+                  PAIEMENT EN LIGNE
+                </div>
+                <div className={"text-card-subtitle"}>
+                  Simplifiez vos achats !
+                </div>
+                <div className={"text-card-content"}>
+                  Facilitez vos commandes en passant par l'application et retirez les simplement dans la pharmacie de votre choix.
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className={"footer"}>
+          <div className={"content-title"}>Nous contacter</div>
+          <div className={"content-subtitle"}>Vous êtes intéressés ou souhaitez plus d'informations sur notre solution ? Envoyez nous un mail !</div>
+        </div>
+        {/*<div className={"utilisateurs"}>
+        </div>
+        <div className={"users_1"}>
+          <div>lel</div>
+
+          <div>lel</div>
+
+        </div>
+
+        <div className={"activités"}>
+        </div>
+        <div className={"fonctionnalités"}>
+        </div>*/}
       </div>
     );
   }
---
+++ src/TestGrid.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import GridLayout from 'react-grid-layout';
+
+export default class TestGrid extends Component {
+    render() {
+        // layout is an array of objects, see the demo for more complete usage
+        let layout = [
+            {i: 'a', x: 0, y: 0, w: 1, h: 2, static: true},
+            {i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4},
+            {i: 'c', x: 4, y: 0, w: 1, h: 2},
+            {i: 'd', x: 1, y: 1, w: 1, h: 2}
+        ];
+        return (
+            <GridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
+                <div key="a">a</div>
+                <div key="b">b</div>
+                <div key="c">c</div>
+                <div key="d">d</div>
+            </GridLayout>
+        )
+    }
+}
